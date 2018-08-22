bd71d35799ce56bcaeacb57b742f59a74c140d5e
Jinyong Kim <o2.3ys.v.er@gmail.com>
2018-08-22 08:44:08 +0200

    change many thins

--- app/controllers/restaurants/reservations_controller.rb
+++ app/controllers/restaurants/reservations_controller.rb
@@ -4,10 +4,30 @@ class Restaurants::ReservationsController < ApplicationController
     @reservations = Reservation.all
   end
 
+  def history
+  end
+
   def show
+    raise
     skip_policy_scope
     @reservation = Reservation.find(params[:id])
     @customer = Customer.find(@reservation.customer_id)
   end
 
+  def edit
+    skip_policy_scope
+    @reservation = Reservation.find(params[:id])
+    @restaurant = current_restaurant
+  end
+
+  def update
+    skip_policy_scope
+    @reservation = Reservation.find(params[:id])
+    @restaurant.update(reservation_params)
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:status)
+  end
 end
+
